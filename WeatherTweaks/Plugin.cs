c40a66d4600692d7df6c94e86db7abdcb340d741
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-01-17 18:59:47 +0100

    initial

---
+++ WeatherTweaks/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace WeatherTweaks
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        internal static ManualLogSource logger;
+
+        private void Awake()
+        {
+
+            logger = Logger;
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
