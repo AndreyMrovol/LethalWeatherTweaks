3793d93fd0df79bc76a4cbfd0871934fb972f4ee
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-01-29 15:50:36 +0100

    should mod be active cases

--- WeatherTweaks/Patches/SetPlanetsWeather.cs
+++ WeatherTweaks/Patches/SetPlanetsWeather.cs
@@ -14,13 +14,45 @@ namespace WeatherTweaks
     {
       Plugin.logger.LogMessage("SetPlanetsWeather called.");
 
+      // there are 3 possible cases:
+      // we're hosting - mod is active, we're syncing weather data
+      // we're a client - mod is active, we're refreshing weathers based on received data
+      // we're a client - mod is not active, we're refreshing weathers based on vanilla data
+
+      // because of how the vanilla mechanic works, weather is set on every client separately from seeded randomness
+      // but this mod needs previous day's weather to calculate current day's weather, so it's not synced on joining
+      // late joining mods are kinda broken - they pull previous weather from ass (and also two times?)
+
+      // based on lobby tag we decide what scenario we're in
+
+      bool isLobby = GameNetworkManager.Instance.currentLobby != null;
+
       if (StartOfRound.Instance.IsHost)
       {
         SetWeathers(__instance, []);
+
+        if (isLobby)
+        {
+          GameNetworkManager.Instance.currentLobby?.SetData("WeatherTweaks", "true");
+        }
       }
       else
       {
-        Plugin.logger.LogMessage("Not a host, waiting for host to update weather data");
+        Plugin.logger.LogMessage("Not a host");
+
+        if (isLobby)
+        {
+          if (GameNetworkManager.Instance.currentLobby?.GetData("WeatherTweaks") != null)
+          {
+            Plugin.logger.LogMessage("Detected mod on host, waiting for weather data");
+          }
+          else
+          {
+            Plugin.logger.LogMessage("Mod not detected on host, falling back to vanilla");
+            return true;
+          }
+        }
+
         Plugin.logger.LogDebug($"Current data: {NetworkedConfig.previousWeatherSynced.Value}");
       }
 
@@ -41,9 +73,6 @@ namespace WeatherTweaks
 
       int seed = __instance.randomMapSeed + 31;
       System.Random random = new System.Random(seed);
-
-      // late joining mods are kinda broken - they pull previous weather from ass (and also two times?)
-      // we need some sort of networked solution to fix this
 
       // Dictionary<string, LevelWeatherType> previousDayWeather = new Dictionary<string, LevelWeatherType>();
       Dictionary<string, LevelWeatherType> vanillaSelectedWeather = VanillaWeathers(0, __instance);
