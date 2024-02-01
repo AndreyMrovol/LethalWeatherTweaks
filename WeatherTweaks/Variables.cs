1f38e642f76ca616a5279b8027e0b4705d941e83
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-02-01 03:16:57 +0100

    logs are cleaner

--- WeatherTweaks/Variables.cs
+++ WeatherTweaks/Variables.cs
@@ -38,7 +38,7 @@ namespace WeatherTweaks
       {
         var weatherType = weather;
         var weatherWeight = weights[weatherType];
-        Plugin.logger.LogDebug($"weather: {weatherType} has weight {weatherWeight}");
+        Plugin.logger.LogDebug($"{weatherType} has weight {weatherWeight}");
 
         for (var i = 0; i < weatherWeight; i++)
         {
