8210b20ba3228a4a06448252ca8a4d07f69728c8
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-02-03 10:53:54 +0100

    all patches are needed for uncertainty

--- WeatherTweaks/UncertainWeather.cs
+++ WeatherTweaks/UncertainWeather.cs
@@ -14,7 +14,7 @@ namespace WeatherTweaks
     {
       uncertainWeathers.Clear();
 
-      if (!ConfigManager.UncertainWeatherEnabled.Value)
+      if (!ConfigManager.UncertainWeatherEnabled.Value || !ConfigManager.TerminalPatchEnabled.Value || !ConfigManager.MapScreenPatch.Value)
       {
         Plugin.logger.LogInfo("Uncertain weathers are disabled.");
         return uncertainWeathers;
