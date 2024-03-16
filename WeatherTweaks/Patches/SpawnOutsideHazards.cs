245b3cb011b41e4f8b6749d7b97d7d4f1295364b
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-03-16 12:03:03 +0100

    Patch RoundManager.SpawnOutsideHazards

---
+++ WeatherTweaks/Patches/SpawnOutsideHazards.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace WeatherTweaks
+{
+  [HarmonyPatch(typeof(RoundManager))]
+  partial class BasegameWeatherPatch
+  {
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(RoundManager), "SpawnOutsideHazards")]
+    static IEnumerable<CodeInstruction> SpawnOutsideHazardsPatch(IEnumerable<CodeInstruction> instructions)
+    {
+      CodeMatcher codeMatcher = new CodeMatcher(instructions);
+
+      codeMatcher = codeMatcher.MatchForward(
+        false,
+        new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(TimeOfDay), "Instance")),
+        new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(TimeOfDay), "currentLevelWeather"))
+      );
+      logger.LogDebug($"Matched Ldfld for RoundManager.SpawnOutsideHazards");
+
+      logger.LogWarning($"Matches found: {codeMatcher.Length}");
+      logger.LogWarning($"Is valid: {codeMatcher.IsValid}");
+
+      codeMatcher.Repeat(match =>
+      {
+        logger.LogWarning($"Matched Ldfld");
+
+        // Remove original instruction
+        codeMatcher.RemoveInstruction(); // removes  call class TimeOfDay  TimeOfDay::get_Instance()
+        codeMatcher.RemoveInstruction(); // removes  ldfld float32 TimeOfDay::currentWeatherVariable
+
+        // Get the current weather variable
+        // Variables.GetLevelWeatherVariable method takes 2 arguments: int and bool - set them
+        codeMatcher.InsertAndAdvance(new CodeInstruction(OpCodes.Ldc_I4, (int)LevelWeatherType.Rainy));
+        codeMatcher.InsertAndAdvance(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Variables), "LevelHasWeather")));
+      });
+
+      return codeMatcher.InstructionEnumeration();
+    }
+  }
+}
