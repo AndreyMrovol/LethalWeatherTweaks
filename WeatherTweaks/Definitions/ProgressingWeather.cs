41f9cef7be1913040b43e8033ca74b36f54c6650
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-03-30 12:43:47 +0100

    show changing weather text longer

--- WeatherTweaks/Definitions/ProgressingWeather.cs
+++ WeatherTweaks/Definitions/ProgressingWeather.cs
@@ -33,7 +33,7 @@ namespace WeatherTweaks.Modules
           {
             speakerText = "Weather Forecast",
             bodyText = $"The weather will be changing to {WeatherType}",
-            waitTime = 3f
+            waitTime = 7f
           }
         ];
       }
