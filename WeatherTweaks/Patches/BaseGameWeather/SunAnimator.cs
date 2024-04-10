e11dbf226c3520e9e32c765b225776d4980cb270
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-04-10 18:35:47 +0200

    SunAnimator fully working version

--- WeatherTweaks/Patches/BaseGameWeather/SunAnimator.cs
+++ WeatherTweaks/Patches/BaseGameWeather/SunAnimator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
+using BepInEx.Logging;
 using DunGen;
 using HarmonyLib;
 using MonoMod.Cil;
@@ -12,6 +13,8 @@ namespace WeatherTweaks
   [HarmonyPatch(typeof(TimeOfDay))]
   partial class BasegameWeatherPatch
   {
+    internal static ManualLogSource loggerSun = BepInEx.Logging.Logger.CreateLogSource("WeatherTweaks SunAnimator");
+
     internal static Dictionary<LevelWeatherType, string> clipNames =
       new()
       {
@@ -31,7 +34,10 @@ namespace WeatherTweaks
       "StarlancerTriskelionSunAnimContainer",
     };
 
+    internal static Animator animator;
     internal static AnimatorOverrideController animatorOverrideController;
+
+    // internal static AnimationClipOverrides clipOverrides;
 
     public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, AnimationClip>>
     {
@@ -55,27 +61,46 @@ namespace WeatherTweaks
       // Right now the possible transitions are:
       // state TimeOfDaySun -> state TimeOfDaySunEclipse (bool eclipsed = true is the condition)
       // state TimeOfDaySun -> state TimeOfDaySunStormy (bool overcast = true is the condition)
-
-      Animator animator = TimeOfDay.Instance.sunAnimator;
-      // animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
-      // AnimationClipOverrides clipOverrides = new(animatorOverrideController.overridesCount);
-
       // i want to change the animation clip of the sun based on the weather type
 
-      // log all clips in the animator
-      animator.runtimeAnimatorController.animationClips.ToList().ForEach(clip => Plugin.logger.LogInfo(clip.name));
+      if (animator == null)
+      {
+        animator = TimeOfDay.Instance.sunAnimator;
+      }
 
       // get the name of the sun animator controller
       string animatorControllerName = animator.runtimeAnimatorController.name;
-      Plugin.logger.LogInfo($"animatorControllerName: {animatorControllerName}, weatherType: {weatherType}");
+      loggerSun.LogInfo($"animatorControllerName: {animatorControllerName}, weatherType: {weatherType}");
 
-      if (!animatorControllerNames.Contains(animatorControllerName))
+      if (!animatorControllerNames.Contains(animatorControllerName) && !animatorControllerName.Contains("override"))
       {
-        Plugin.logger.LogError($"Animator controller {animatorControllerName} not found in list of supported animator controllers");
+        loggerSun.LogError($"Animator controller {animatorControllerName} not found in list of supported animator controllers");
         return;
       }
 
-      var animationClips = animator.runtimeAnimatorController.animationClips.ToList();
+      if (animatorOverrideController == null)
+      {
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController)
+        {
+          name = $"{animatorControllerName}override"
+        };
+      }
+
+      AnimationClipOverrides clipOverrides = new(animatorOverrideController.overridesCount);
+      loggerSun.LogWarning($"Overrides: {animatorOverrideController.overridesCount}");
+      animatorOverrideController.GetOverrides(clipOverrides);
+
+      LogOverrides(clipOverrides);
+
+      // log all clips in the animator
+      var animationClips = animatorOverrideController.runtimeAnimatorController.animationClips.ToList();
+      animationClips.ForEach(clip =>
+      {
+        loggerSun.LogInfo($"clip: {clip.name}");
+        loggerSun.LogInfo($"clip length: {clip.length}");
+        loggerSun.LogInfo($"clip framerate: {clip.frameRate}");
+      });
+
       Dictionary<LevelWeatherType, AnimationClip> clips =
         new()
         {
@@ -96,27 +121,105 @@ namespace WeatherTweaks
       string animationClipName = clips.TryGetValue(weatherType, out AnimationClip clip) ? clip.name : null;
       if (animationClipName == null)
       {
-        Plugin.logger.LogError($"No animation clip found for weather type {weatherType}");
+        loggerSun.LogError($"No animation clip found for weather type {weatherType}");
         return;
       }
 
+      clips
+        .ToList()
+        .ForEach(clipPair =>
+        {
+          // override only clips different than the new one
+
+          if (clipPair.Key != weatherType)
+          {
+            clipOverrides[clipPair.Value.name] = clips[weatherType];
+            loggerSun.LogInfo($"Setting override from {clipPair.Value.name} to {clips[weatherType].name}");
+          }
+          else
+          {
+            clipOverrides[clipPair.Value.name] = null;
+            loggerSun.LogInfo($"Setting override from {clipPair.Value.name} to null");
+          }
+        });
+
       // get the hash of the animation clip
       int animationClipHash = Animator.StringToHash(animationClipName);
+      loggerSun.LogInfo($"animationClipHash: {animationClipHash}");
 
-      Plugin.logger.LogInfo($"Changing animation clip to {animationClipName} ({animationClipHash})");
-      Plugin.logger.LogInfo($"Current bools: {animator.GetBool("overcast")} {animator.GetBool("eclipsed")}");
+      // animationClips.ForEach(clip =>
+      // {
+      //   clipOverrides[clip.name] = clips[weatherType];
+
+      //   loggerSun.LogInfo($"Setting override from {clip.name} to {clips[weatherType]}");
+      // });
+
+      loggerSun.LogInfo($"Changing animation clip to {animationClipName} ({animationClipHash})");
+      loggerSun.LogInfo($"Current bools: {animator.GetBool("overcast")} {animator.GetBool("eclipsed")}");
+
+      // // set the overrides
+      // if (weatherType == LevelWeatherType.None)
+      // {
+      //   loggerSun.LogInfo($"Setting {animationClipName} to {clips[LevelWeatherType.None].name}");
+      //   clipOverrides[animationClipName] = clips[LevelWeatherType.None];
+      // }
+      // else if (weatherType == LevelWeatherType.Stormy)
+      // {
+      //   loggerSun.LogInfo($"Setting animation clip to {clips[LevelWeatherType.Stormy].name}");
+      //   clipOverrides[animationClipName] = clips[LevelWeatherType.Stormy];
+      // }
+      // else if (weatherType == LevelWeatherType.Eclipsed)
+      // {
+      //   clipOverrides[animationClipName] = clips[LevelWeatherType.Eclipsed];
+      // }
+
+      LogOverrides(clipOverrides);
+
+      if (weatherType != LevelWeatherType.None)
+      {
+        animatorOverrideController.ApplyOverrides(clipOverrides);
+        animator.runtimeAnimatorController = animatorOverrideController;
+      }
+      else
+      {
+        animator.runtimeAnimatorController = animatorOverrideController.runtimeAnimatorController;
+      }
+
+      // animator.CrossFadeInFixedTime(animationClipHash, 2.5f, 0);
+      // animator.Play(animationClipHash, 0, 0);
+
+      // log the current clip name
+      loggerSun.LogInfo($"Current clip: {animator.GetCurrentAnimatorClipInfo(0)[0].clip.name}");
+
+      // if (weatherType == LevelWeatherType.None)
+      // {
+      //   animator.Play(0, 1, 0);
+      // }
+      // else
+      // {
+      // }
 
       // set the animation clip with 2s transition
-      animator.CrossFadeInFixedTime(animationClipHash, 2.5f, 0);
       // animator.Play(animationClipHash, 0, 0);
+    }
+
+    internal static void LogOverrides(AnimationClipOverrides clipOverrides)
+    {
+      loggerSun.LogWarning($"Overrides: {clipOverrides.Count}");
+      clipOverrides
+        .ToList()
+        .ForEach(clip =>
+        {
+          loggerSun.LogInfo($"clip {(clip.Key ? clip.Key.name : "null")} : {(clip.Value ? clip.Value.name : "null")}");
+        });
     }
 
     // [HarmonyPatch("Update")]
     // [HarmonyPostfix]
     // public static void UpdatePatch()
     // {
-    //   Plugin.logger.LogInfo($"{TimeOfDay.Instance.sunAnimator.GetFloat("timeOfDay")}");
-    //   Plugin.logger.LogInfo($"{TimeOfDay.Instance.sunIndirect.intensity} {TimeOfDay.Instance.sunDirect.intensity}");
+    //   loggerSun.LogInfo($"{TimeOfDay.Instance.sunAnimator.GetFloat("timeOfDay")}");
+    //   loggerSun.LogInfo($"{TimeOfDay.Instance.sunIndirect.intensity} {TimeOfDay.Instance.sunDirect.intensity}");
     // }
   }
 }
