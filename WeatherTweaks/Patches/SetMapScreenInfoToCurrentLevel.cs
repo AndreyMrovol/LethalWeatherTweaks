b86dc28b5f70db3e56643785ba21ad84629523ea
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-04-17 18:09:26 +0200

    new coloring algorithm

--- WeatherTweaks/Patches/SetMapScreenInfoToCurrentLevel.cs
+++ WeatherTweaks/Patches/SetMapScreenInfoToCurrentLevel.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using HarmonyLib;
 using Newtonsoft.Json;
 using TMPro;
@@ -21,14 +22,27 @@ namespace WeatherTweaks
         return;
       }
 
+      if (Variables.CurrentWeathers == null || Variables.CurrentWeathers.Count == 0)
+      {
+        Plugin.logger.LogWarning("CurrentWeathers is null, cannot display weather");
+        return;
+      }
+
+      Plugin.logger.LogWarning(
+        $"Color: {___screenLevelDescription.color}, {___screenLevelDescription.colorGradient}, {___screenLevelDescription.colorGradientPreset}, {___screenLevelDescription.faceColor}, {___screenLevelDescription.outlineColor}"
+      );
+
       string weatherCondition = Variables.GetPlanetCurrentWeather(___currentLevel);
 
       Plugin.logger.LogDebug($"Weather condition: {weatherCondition}");
 
       StringBuilder stringBuilder = new();
       stringBuilder.Append("ORBITING: " + ___currentLevel.PlanetName + "\n");
-      stringBuilder.Append("WEATHER: " + $"{GetHexColor(weatherCondition)}{weatherCondition}</color>" + "\n");
+      // stringBuilder.Append("WEATHER: " + $"{GetHexColor(weatherCondition)}{weatherCondition}</color>" + "\n");
+      stringBuilder.Append($"WEATHER: {GetColoredString(___currentLevel)}\n");
       stringBuilder.Append(___currentLevel.LevelDescription ?? "");
+
+      ___screenLevelDescription.fontWeight = FontWeight.Bold;
       ___screenLevelDescription.text = stringBuilder.ToString();
     }
 
@@ -88,6 +102,68 @@ namespace WeatherTweaks
       };
 
       return "<color=#" + pickedColor + ">";
+    }
+
+    private static string GetColoredString(SelectableLevel level)
+    {
+      WeatherType currentWeather = Variables.GetPlanetCurrentWeatherType(level);
+      string currentWeatherString = Variables.GetPlanetCurrentWeather(level);
+
+      bool uncertain = currentWeather.Name == currentWeatherString;
+
+      if (currentWeatherString == "[UNKNOWN]")
+      {
+        return "<color=#4a4a4a>[UNKNOWN]</color>";
+      }
+
+      if (!ConfigManager.ColoredWeathers.Value)
+      {
+        return currentWeatherString;
+      }
+
+      // string outputString = currentWeatherString;
+      string outputString = "";
+      Regex split = new(@"(\/)|(\?)|(>)");
+
+      split
+        .Split(currentWeatherString)
+        .ToList()
+        .ForEach(word =>
+        {
+          Plugin.logger.LogDebug($"Word: {word}");
+          string newWord = word.Trim();
+
+          // create a method to resolve each individual word to a weather color
+          // without using LevelWeatherType *because* it's not gonna resolve when the word is not a weather
+
+          string pickedColor = newWord switch
+          {
+            "Testing" => "BA089C",
+            "Eclipsed" => "FF0000",
+            "Flooded" => "FF9300",
+            "Stormy" => "FF9300",
+            "Foggy" => "FFDC00",
+            "Rainy" => "FFDC00",
+            "DustClouds" => "69FF6B",
+            "None" => "69FF6B",
+            "+" => "FFFFFF",
+            ">" => "FFFFFF",
+            "/" => "FFFFFF",
+            _ => "000000",
+          };
+
+          // if (word == ">")
+          // {
+          //   newWord = "⇨";
+          //   sectionToReplace = new(@">");
+          // }
+
+          // outputString = outputString.Replace(word, pickedColor != "000000" ? $"<color=#{pickedColor}>{newWord}</color>" : $"{word}");
+          outputString += pickedColor != "000000" ? $"<color=#{pickedColor}>{word}</color>" : $"{newWord}";
+        });
+
+      Plugin.logger.LogWarning($"Output: {outputString}");
+      return outputString;
     }
   }
 }
