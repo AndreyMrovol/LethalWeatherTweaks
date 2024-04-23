1fae67dd955660a512fc565b418b438984b52525
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-04-23 14:35:29 +0200

    i will have to merge it again later and i'll regret this moment

--- WeatherTweaks/Plugin.cs
+++ WeatherTweaks/Plugin.cs
@@ -23,16 +23,6 @@ namespace WeatherTweaks
 
       GeneralImprovementsWeather.Init();
 
-      if (Chainloader.PluginInfos.ContainsKey("com.malco.lethalcompany.moreshipupgrades"))
-      {
-        Patches.LateGameUpgrades.Init();
-      }
-
-      if (Chainloader.PluginInfos.ContainsKey("com.github.fredolx.meteomultiplier"))
-      {
-        Patches.MeteoMultiplierPatches.Init();
-      }
-
       logger.LogInfo(
         @"
                   .::.                  
