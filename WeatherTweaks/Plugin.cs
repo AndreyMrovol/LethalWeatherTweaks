debf6ac839fa10f15e823e2e338c024bae253dd4
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-12-28 22:54:20 +0100

    move weathers into a separate mod

--- WeatherTweaks/Plugin.cs
+++ WeatherTweaks/Plugin.cs
@@ -73,46 +73,6 @@ namespace WeatherTweaks
         LobbyCompatibilityCompatibility.Init();
       }
 
-      Weather cloudyWeather =
-        new("Cloudy", new(null, null) { SunAnimatorBool = "overcast" })
-        {
-          Color = new(r: 0, g: 1f, b: 0.55f, a: 1),
-          Config =
-          {
-            ScrapAmountMultiplier = new(1.6f),
-            ScrapValueMultiplier = new(0.8f),
-            WeatherToWeatherWeights = new(["Eclipsed@50", "Stormy@80"]),
-            DefaultWeight = new(25),
-          },
-        };
-      WeatherRegistry.WeatherManager.RegisterWeather(cloudyWeather);
-
-      GameObject blackoutObject = GameObject.Instantiate(new GameObject() { name = "BlackoutWeather" });
-      blackoutObject.hideFlags = HideFlags.HideAndDontSave;
-      blackoutObject.AddComponent<Weathers.Blackout>();
-      GameObject.DontDestroyOnLoad(blackoutObject);
-
-      var BlackoutAssets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Info.Location), "blackout"));
-      AnimationClip blackoutClip = BlackoutAssets.LoadAsset<AnimationClip>("BlackoutSunClip");
-
-      Weather Blackout =
-        new("Blackout", new(null, blackoutObject) { SunAnimatorBool = "eclipse", })
-        {
-          // in case i ever forget: screen is fucking green, so green channel *has to* have 20% less value to be gray
-          Color = new(r: 0.5f, g: 0.4f, b: 0.5f, a: 1),
-          Config =
-          {
-            ScrapAmountMultiplier = new(0.65f),
-            ScrapValueMultiplier = new(1.7f),
-            DefaultWeight = new(25),
-            LevelWeights = new("Rend@200; Dine@200; Titan@200"),
-            WeatherToWeatherWeights = new("None@200; Cloudy@250")
-          },
-          AnimationClip = blackoutClip
-        };
-      WeatherRegistry.WeatherManager.RegisterWeather(Blackout);
-      BlackoutWeather = Blackout;
-
       Init.InitMethod();
 
       logger.LogInfo(
