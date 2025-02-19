bd09eed977aec87824b808f2e96d252257d601d7
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-02-19 16:55:15 +0100

    formatting pass

--- WeatherTweaks/ChangeMidDay.cs
+++ WeatherTweaks/ChangeMidDay.cs
@@ -114,7 +114,7 @@ namespace WeatherTweaks
       GameNetworkManager.Instance.localPlayerController.currentAudioTrigger.weatherEffect = (int)fullWeatherType.VanillaWeatherType;
 
       CurrentEntry = entry;
-    
+
       if (entry.Weather.WeatherType == LevelWeatherType.Rainy)
       {
         TimeOfDay.Instance.StartCoroutine(SpawnMudPatches());
@@ -168,52 +168,59 @@ namespace WeatherTweaks
 
     internal static bool IsMudPickValid(Vector3 position)
     {
-        if (position == default)
-            return false;
-        float mudSqrDistance = 100f; //Squared distance between possible mud location and a player
-        bool isValidPick = true;
-        foreach (PlayerControllerB player in StartOfRound.Instance.allPlayerScripts)
+      if (position == default)
+      {
+        return false;
+      }
+      float mudSqrDistance = 100f; //Squared distance between possible mud location and a player
+      bool isValidPick = true;
+      foreach (PlayerControllerB player in StartOfRound.Instance.allPlayerScripts)
+      {
+        if (Vector3.SqrMagnitude(position - player.transform.position) < mudSqrDistance)
         {
-            if (Vector3.SqrMagnitude(position - player.transform.position) < mudSqrDistance)
-            {
-                isValidPick = false;
-                break;
-            }
+          isValidPick = false;
+          break;
         }
-        return isValidPick;
+      }
+      return isValidPick;
     }
 
     internal static Vector3 TryGetValidMudPick(System.Random seededRandom, NavMeshHit navHit)
     {
-        Vector3 mudPosition = default;
-        int attemptNum = 0;
-        int maxMudPlacementAttempts = 10;
-        while (attemptNum < maxMudPlacementAttempts && !IsMudPickValid(mudPosition))
-        {
-            mudPosition = RoundManager.Instance.outsideAINodes[seededRandom.Next(0, RoundManager.Instance.outsideAINodes.Length)].transform.position;
-            mudPosition = RoundManager.Instance.GetRandomNavMeshPositionInBoxPredictable(mudPosition, 30f, navHit, seededRandom, -1) + Vector3.up;
-            attemptNum++;
-        }
+      Vector3 mudPosition = default;
+      int attemptNum = 0;
+      int maxMudPlacementAttempts = 10;
+      while (attemptNum < maxMudPlacementAttempts && !IsMudPickValid(mudPosition))
+      {
+        mudPosition = RoundManager.Instance.outsideAINodes[seededRandom.Next(0, RoundManager.Instance.outsideAINodes.Length)].transform.position;
+        mudPosition = RoundManager.Instance.GetRandomNavMeshPositionInBoxPredictable(mudPosition, 30f, navHit, seededRandom, -1) + Vector3.up;
+        attemptNum++;
+      }
 
-        return mudPosition;
+      return mudPosition;
     }
 
     internal static IEnumerator SpawnMudPatches()
     {
-        logger.LogDebug("Spawning mud patches!");
-        System.Random seededRandom = new(StartOfRound.Instance.randomMapSeed + 2);
-        NavMeshHit navHit = new();
-        int numberOfPuddles = random.Next(5, 15);
-        if (random.Next(0, 100) < 7)
-        {
-            numberOfPuddles = random.Next(5, 30);
-        }
-        for (int i = 0; i < numberOfPuddles; i++)
-        {
-            Vector3 mudPosition = TryGetValidMudPick(seededRandom, navHit) + Vector3.up;
-            GameObject.Instantiate(RoundManager.Instance.quicksandPrefab, mudPosition, Quaternion.identity, RoundManager.Instance.mapPropsContainer.transform);
-            yield return null;
-        }
+      logger.LogDebug("Spawning mud patches!");
+      System.Random seededRandom = new(StartOfRound.Instance.randomMapSeed + 2);
+      NavMeshHit navHit = new();
+      int numberOfPuddles = random.Next(5, 15);
+      if (random.Next(0, 100) < 7)
+      {
+        numberOfPuddles = random.Next(5, 30);
+      }
+      for (int i = 0; i < numberOfPuddles; i++)
+      {
+        Vector3 mudPosition = TryGetValidMudPick(seededRandom, navHit) + Vector3.up;
+        GameObject.Instantiate(
+          RoundManager.Instance.quicksandPrefab,
+          mudPosition,
+          Quaternion.identity,
+          RoundManager.Instance.mapPropsContainer.transform
+        );
+        yield return null;
+      }
     }
   }
 }
