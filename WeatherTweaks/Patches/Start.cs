e9fcb8ff79c5d7928cac01e948a46db7240d6a60
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-02-25 12:22:36 +0100

    add special symbols to another pool

--- WeatherTweaks/Patches/Start.cs
+++ WeatherTweaks/Patches/Start.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 namespace WeatherTweaks.Patches
@@ -8,10 +9,20 @@ namespace WeatherTweaks.Patches
     {
       Variables.CurrentEffects.Clear();
 
-      WeatherRegistry.Settings.ScreenMapColors.Add("+", WeatherRegistry.Utils.ColorConverter.ToTMPColorGradient(Color.white));
-      WeatherRegistry.Settings.ScreenMapColors.Add("/", WeatherRegistry.Utils.ColorConverter.ToTMPColorGradient(Color.white));
-      WeatherRegistry.Settings.ScreenMapColors.Add(">", WeatherRegistry.Utils.ColorConverter.ToTMPColorGradient(Color.white));
-      WeatherRegistry.Settings.ScreenMapColors.Add("?", WeatherRegistry.Utils.ColorConverter.ToTMPColorGradient(Color.white));
+      TMP_ColorGradient specialSymbolGradient = WeatherRegistry.Utils.ColorConverter.ToTMPColorGradient(Color.white);
+
+      WeatherRegistry.Settings.ScreenMapColors.Add("+", specialSymbolGradient);
+      WeatherRegistry.Settings.ScreenMapColors.Add("plus", specialSymbolGradient);
+
+      WeatherRegistry.Settings.ScreenMapColors.Add("/", specialSymbolGradient);
+      WeatherRegistry.Settings.ScreenMapColors.Add("slash", specialSymbolGradient);
+
+      WeatherRegistry.Settings.ScreenMapColors.Add(">", specialSymbolGradient);
+      WeatherRegistry.Settings.ScreenMapColors.Add("arrow", specialSymbolGradient);
+
+      WeatherRegistry.Settings.ScreenMapColors.Add("?", specialSymbolGradient);
+      WeatherRegistry.Settings.ScreenMapColors.Add("question", specialSymbolGradient);
+
       WeatherRegistry.Settings.ScreenMapColors.Add(
         "[UNKNOWN]",
         WeatherRegistry.Utils.ColorConverter.ToTMPColorGradient(new Color(0.29f, 0.29f, 0.29f))
